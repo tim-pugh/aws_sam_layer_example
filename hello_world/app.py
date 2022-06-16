45664ee20d6ccf300f4123b9cbc861864be5ef74
Tim Pugh <22535268+tim-pugh@users.noreply.github.com>
2022-06-16 14:11:41 -0700

    Update Lambda Observability

--- hello_world/app.py
+++ hello_world/app.py
@@ -1,44 +1,48 @@
+# Author: Tim Pugh
+# References:
+# Why You Should Never, Ever print() in a Lambda Function - https://towardsdatascience.com/why-you-should-never-ever-print-in-a-lambda-function-f997d684a705
+# Instrumenting Python code in AWS Lambda - https://docs.aws.amazon.com/lambda/latest/dg/python-tracing.html and its updated source code https://github.com/awsdocs/aws-lambda-developer-guide/blob/main/sample-apps/blank-python/function/lambda_function.py
+# AWS X-Ray SDK for Python - https://docs.aws.amazon.com/xray/latest/devguide/xray-sdk-python.html
+import os
 import json
+import boto3
+import logging
+import jsonpickle
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.core import patch_all
 
-# import requests
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+patch_all()
+
+# example of building clients above/outside the Lambda handler
+#client = boto3.client('lambda')
+# client.get_account_settings()
 
 
 def lambda_handler(event, context):
-    """Sample pure Lambda function
+    try:
+        logger.info(f'## ENVIRONMENT VARIABLES\r' +
+                    jsonpickle.encode(dict(**os.environ)))
+        logger.info(f'## EVENT\r' + jsonpickle.encode(event))
+        logger.info(f'## CONTEXT\r' + jsonpickle.encode(context))
 
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
+        return {
+            "statusCode": 200,
+            "body": json.dumps(
+                {
+                    "message": "hello world",
+                }
+            ),
+        }
 
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps(
-            {
-                "message": "hello world",
-                # "location": ip.text.replace("\n", "")
-            }
-        ),
-    }
+    except Exception as exp:
+        exception_type, exception_value, exception_traceback = sys.exc_info()
+        traceback_string = traceback.format_exception(
+            exception_type, exception_value, exception_traceback)
+        err_msg = json.dumps({
+            "errorType": exception_type.__name__,
+            "errorMessage": str(exception_value),
+            "stackTrace": traceback_string
+        })
+        logger.error(err_msg)
